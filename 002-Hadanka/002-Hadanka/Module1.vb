45824c32feb15ed22dc60864069c744fe2bd4a1a
Jozef Hollý <j2.00ghz@gmail.com>
2014-09-25 09:15:27 +0200

    Base

---
+++ 002-Hadanka/002-Hadanka/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+    Dim n As Integer
+    Dim i As Integer = 2
+    Sub Main()
+        n = Console.ReadLine()
+        While Not n Mod i = 0
+            i += 1
+        End While
+        Console.WriteLine("Nejmensi delitel je: " & i)
+        Console.ReadKey()
+    End Sub
+
+End Module
