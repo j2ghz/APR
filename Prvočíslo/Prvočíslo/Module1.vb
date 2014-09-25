4273c98d02903ef91cb9c705e3673273bf313f9a
Jozef Hollý <j2.00ghz@gmail.com>
2014-09-25 08:46:47 +0200

    Base

---
+++ Prvočíslo/Prvočíslo/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+    Dim n As Integer
+    Dim prvocislo As Boolean = True
+    Sub Main()
+        n = Console.ReadLine
+        For i As Integer = 2 To Math.Round(Math.Sqrt(n), MidpointRounding.AwayFromZero) Step 1
+            If n Mod i = 0 Then
+                prvocislo = False
+                Exit For
+            End If
+        Next
+        Console.WriteLine(prvocislo)
+    End Sub
+
+End Module
