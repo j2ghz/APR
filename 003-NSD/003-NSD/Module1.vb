c95b7d5d5d564c889f51a1ab02189d07248ad35d
Jozef Hollý GfW NTB <j2.00ghz@gmail.com>
2014-10-02 09:23:40 +0200

    003-NSD a) zkouseni

---
+++ 003-NSD/003-NSD/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+    Dim a, b As Integer
+    Sub Main()
+        Console.Write("a=")
+        a = Console.ReadLine
+        Console.Write("b=")
+        b = Console.ReadLine
+        Console.WriteLine("(Z)koušení nebo (E)uklidova metoda?")
+        Select Case Console.ReadLine.ToLower
+            Case "z"
+                zkouseni()
+            Case "e"
+
+        End Select
+    End Sub
+    Sub zkouseni()
+        For i = Math.Min(a, b) To 1 Step -1
+            If a Mod i = 0 AndAlso b Mod i = 0 Then
+                Console.WriteLine(i)
+                Console.ReadKey()
+                Exit Sub
+            End If
+        Next
+    End Sub
+End Module
