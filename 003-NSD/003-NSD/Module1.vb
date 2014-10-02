34a600547ec709cb5326adfa10eea8af99d745d3
Jozef Hollý GfW NTB <j2.00ghz@gmail.com>
2014-10-02 09:33:33 +0200

    003-NSD b)euklidova

--- 003-NSD/003-NSD/Module1.vb
+++ 003-NSD/003-NSD/Module1.vb
@@ -10,7 +10,7 @@
             Case "z"
                 zkouseni()
             Case "e"
-
+                euklidova()
         End Select
     End Sub
     Sub zkouseni()
@@ -21,5 +21,17 @@
                 Exit Sub
             End If
         Next
+    End Sub
+    Sub euklidova()
+        If a < b Then
+            Dim tmp = a
+            a = b
+            b = tmp
+        End If
+        While Not a Mod b = 0
+            b = a Mod b
+        End While
+        Console.WriteLine(b)
+        Console.ReadKey()
     End Sub
 End Module
