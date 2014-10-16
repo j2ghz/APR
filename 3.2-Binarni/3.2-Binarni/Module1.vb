8dfcd80aae5612c50f1d6d39fe57e6348f099195
J2ghz NTB <j2.00ghz@gmail.com>
2014-10-16 09:22:13 +0200

    prevod z dvojkove a do dvojkove

---
+++ 3.2-Binarni/3.2-Binarni/Module1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Text.RegularExpressions
+
+Module Module1
+
+    Sub Main()
+        Console.Write("Zadejde číslo pro převod z do dvojkove, nebo d pro prevod z dvojkove: ")
+        Dim tmp As String = Console.ReadLine
+        Select Case IsNumeric(tmp)
+            Case True
+                Console.WriteLine(Convert.ToString(CInt(tmp), 2))
+            Case False
+                Console.Write("Zadejte číslo v dvojkove: ")
+                tmp = Console.ReadLine
+                Console.WriteLine(Convert.ToInt64(tmp, 2))
+        End Select
+        Console.ReadKey()
+
+    End Sub
+
+End Module
