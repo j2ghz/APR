438cda0939ee5111a240bee91bb84db2f7225c44
Jozef Hollý <j2ghz@users.noreply.github.com>
2014-10-30 09:22:07 +0100

    Final

--- 4.1-Odevzdejte-program-na-v-po-et-exp-x-pomoc-sou-tu-nekone-n-ady/module1.vb
+++ 4.1-Odevzdejte-program-na-v-po-et-exp-x-pomoc-sou-tu-nekone-n-ady/module1.vb
@@ -1,16 +1,24 @@
 Module Module1
   Sub Main()
+  Try
+	Console.Write("Zadejte mocninu pro výpočet Eulerova čísla: ")
     Dim x as double = Cdbl(Console.readline)
     Dim e as double = 1+x
-    Dim i = 2
+    Dim i = 3
     dim f = 2
     dim eps
+	dim n = x*x
     Do
-        eps=(x^i)/(f)
+        eps=(n)/(f)
         e+=eps
         f=f*i
-        i+=1
-        Console.writeline(e)
-    Loop while eps>0.0001
+		x*=x
+		i+=1
+    Loop While eps>0.0001
+	Console.writeline(e)
+	Console.readkey
+	Catch e as Exception
+		msgbox(e.ToString,MsgBoxStyle.Critical,"ERROR " & err.number & " - " & err.description)
+	end try
   End Sub
 End Module
