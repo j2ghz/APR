a450cd5ea0d6c47a9a7ee2baeaaf646d0245d4c1
Jozef Hollý <j2ghz@users.noreply.github.com>
2014-10-30 08:43:48 +0100

    Update module1.vb

--- 4.1-Odevzdejte-program-na-v-po-et-exp-x-pomoc-sou-tu-nekone-n-ady/module1.vb
+++ 4.1-Odevzdejte-program-na-v-po-et-exp-x-pomoc-sou-tu-nekone-n-ady/module1.vb
@@ -1,5 +1,14 @@
 Module Module1
   Sub Main()
-  
+    Dim x as double = Cdbl(Console.readline)
+    Dim e as double = 1+x
+    Dim i = 2
+    dim f = 2
+    While true
+        e+=(x^i)/(f)
+        f=f*i
+        i+=1
+        Console.writeline(e)
+    End while
   End Sub
 End Module
