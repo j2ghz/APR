d05f006e8161189ac1234c5c866647819751835c
Jozef Hollý GfwNITRO <j2.00ghz@gmail.com>
2014-12-11 08:10:28 +0100

    5.3

---
+++ 5.2/module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+	Sub Main()
+		Console.WriteLine("Prvky oddělujte mezerami pak enter")
+		dim pole() = console.readline().split(" ")
+		for i = 0 to pole.length-2
+			if pole(i) > pole(i+1) then
+				console.writeline("Neni seřazena")
+				exit sub
+			end if
+		next
+		console.writeline("Je seřazena")
+	End Sub
+End Module
---
+++ 5.3/5.3/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+    Sub Main()
+        Console.WriteLine("Prvky oddělujte mezerami pak enter")
+        Dim pole() = Console.ReadLine().Split(" ")
+        Dim pole2 As New List(Of Integer)
+        For Each s As String In pole
+            pole2.Add(CInt(s))
+        Next
+        pole2.Remove(pole2.Max)
+        Console.WriteLine(pole2.Max)
+    End Sub
+End Module
