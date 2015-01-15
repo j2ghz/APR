4b11e5798ed7f04e812abe681dd068731a35061c
Jozef Hollý GfwNITRO <j2.00ghz@gmail.com>
2015-01-15 08:21:33 +0100

    pokracovani 6

--- 6 magctv/6 magctv/Module1.vb
+++ 6 magctv/6 magctv/Module1.vb
@@ -3,7 +3,20 @@
     Sub Main()
         Console.Write("Zadejte n:")
         Dim n As Integer = Console.ReadLine()
-
+        ReDim pole(n + 1, n + 1)
+        Dim k, j As Integer
+        k = n 'radek
+        j = (n + 1) / 2 'sloupec
+        For i As Integer = 0 To n * n - 1
+            pole((k + i) Mod n, (j - i) Mod n) = i + 1
+        Next
+        For k = 1 To n
+            For j = 1 To n
+                Console.Write(pole(k, j) & "    ")
+            Next
+            Console.WriteLine()
+        Next
+        Console.ReadKey()
     End Sub
 
 End Module
