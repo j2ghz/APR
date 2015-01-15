3b828f5b45adaca6d309ad1fc8fc48ea3053dc0e
Jozef Hollý GfwNITRO <j2.00ghz@gmail.com>
2015-01-15 08:25:43 +0100

    zacatek 6.1-3

---
+++ 6.1-3 výpočet řádkových, sloupcových a diagonálových součtů/6.1-3 výpočet řádkových, sloupcových a diagonálových součtů/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("6.1-3 výpočet řádkových, sloupcových a diagonálových součtů")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("6.1-3 výpočet řádkových, sloupcových a diagonálových součtů")>
+<Assembly: AssemblyCopyright("Copyright ©  2015")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("65a1db29-1a02-4b59-8877-04c4c2ba45d3")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
