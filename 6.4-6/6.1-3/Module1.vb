905caad1d851a1f8be21dc865311a05866644338
Jozef Hollý GfW V17nitro <j2.00ghz@gmail.com>
2015-01-22 08:47:49 +0100

    6.5 hotovo

--- 6.4-6/6.1-3/Module1.vb
+++ 6.4-6/6.1-3/Module1.vb
@@ -1,8 +1,9 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.Write("Zadejte počet řádků:") : Dim radky As Integer = Console.ReadLine
-        Console.Write("Zadejte počet sloupců:") : Dim sloupce As Integer = Console.ReadLine
+        Console.Write("Zadejte počet řádků a sloupců:") : Dim radky As Integer = Console.ReadLine
+        'Console.Write("Zadejte počet sloupců:") : 
+        Dim sloupce As Integer = radky
         Dim matice(radky, sloupce) As Integer
         For radek As Integer = 1 To radky
             For sloupec As Integer = 1 To sloupce
@@ -20,14 +21,13 @@
             Next
             Console.WriteLine()
         Next
-        Console.Write("Který řádek vyměnit? ") : Dim Vradek As Integer = Console.ReadLine
-        Console.Write("S kterým? ") : Dim Vradek2 As Integer = Console.ReadLine
         Dim matice2(radky, sloupce) As Integer
         For radek As Integer = 1 To radky
             For sloupec As Integer = 1 To sloupce
-                matice2(radek, sloupec) = matice(IIf(radek = Vradek, Vradek2, IIf(radek = Vradek2, Vradek, radek)), sloupec)
+                matice2(sloupec, radek) = matice(radek, sloupec)
             Next
         Next
+        Console.WriteLine()
         For radek As Integer = 1 To radky
             For sloupec As Integer = 1 To sloupce
                 Console.Write(matice2(radek, sloupec) & IIf(sloupec = sloupce, "", ","))
