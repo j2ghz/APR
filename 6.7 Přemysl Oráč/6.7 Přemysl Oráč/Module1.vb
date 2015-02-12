4ab8e3136e5975d55b3fe10345b30f15afad0b5b
Jozef Hollý v17nitro <j2.00ghz@gmail.com>
2015-02-12 08:47:46 +0100

    import 6.7

---
+++ 6.7 Přemysl Oráč/6.7 Přemysl Oráč/Module1.vb
@@ -0,0 +1,61 @@
+﻿Module Module1
+    Enum _smer
+        up
+        down
+        left
+        right
+    End Enum
+    Sub Main()
+        Console.Write("Zadejte počet řádků a sloupců:") : Dim radky As Integer = Console.ReadLine
+        'Console.Write("Zadejte počet sloupců:") : 
+        Dim sloupce As Integer = radky
+        Dim matice(radky, sloupce) As Integer
+        Console.Clear()
+        Dim r As Integer = 1
+        Dim s As Integer = 1
+        Dim i = 1
+        Dim smer As _smer = _smer.right
+        While i < sloupce
+            Select Case smer
+                Case _smer.right
+                    matice(r, s) = i
+                    i += 1
+                    s += 1
+                    If s = radky Then
+                        smer = _smer.down
+                    End If
+                Case _smer.down
+                    matice(r, s) = i
+                    i += 1
+                    r += 1
+                    If r = radky Then
+                        smer = _smer.left
+                    End If
+                Case _smer.left
+                    matice(r, s) = i
+                    i += 1
+                    s -= 1
+                    If s = radky Then
+                        smer = _smer.up
+                    End If
+                Case _smer.up
+                    matice(r, s) = i
+                    i += 1
+                    r -= 1
+                    If s = radky Then
+                        smer = _smer.right
+                        radky -= 1
+                    End If
+            End Select
+        End While
+        For radek As Integer = 1 To radky
+            For sloupec As Integer = 1 To sloupce
+                Console.Write(matice(radek, sloupec) & IIf(sloupec = sloupce, "", ","))
+            Next
+            Console.WriteLine()
+        Next
+        Console.ReadKey()
+    End Sub
+
+
+End Module
--- 7.2/7.2/Module1.vb
+++ 7.2/7.2/Module1.vb
@@ -6,19 +6,22 @@
             Dim e As Double = 1 + x
             Dim i = 3
             Dim f = 2
-            Dim eps
+            Dim eps = 0
             Dim n = x * x
             Do
-                eps = (n) / (f)
-                e += eps
-                f = f * i
-                x *= x
-                i += 1
+                pripocitej(eps, n, f, e, i, x)
             Loop While eps > 0.0001
             Console.WriteLine(e)
             Console.ReadKey()
         Catch e As Exception
             MsgBox(e.ToString, MsgBoxStyle.Critical, "ERROR " & Err.Number & " - " & Err.Description)
         End Try
+    End Sub
+    Sub pripocitej(ByRef eps, ByRef n, ByRef f, ByRef e, ByRef i, ByRef x)
+        eps = (n) / (f)
+        e += eps
+        f = f * i
+        x *= x
+        i += 1
     End Sub
 End Module
