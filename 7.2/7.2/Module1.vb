ce5df5df4b947e09e14a1f6d45eeaf8a9710da34
Jozef Hollý v17nitro <j2.00ghz@gmail.com>
2015-02-12 08:34:41 +0100

    import 7.2

---
+++ 7.2/7.2/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+    Sub Main()
+        Try
+            Console.Write("Zadejte mocninu pro výpočet Eulerova čísla: ")
+            Dim x As Double = CDbl(Console.ReadLine)
+            Dim e As Double = 1 + x
+            Dim i = 3
+            Dim f = 2
+            Dim eps
+            Dim n = x * x
+            Do
+                eps = (n) / (f)
+                e += eps
+                f = f * i
+                x *= x
+                i += 1
+            Loop While eps > 0.0001
+            Console.WriteLine(e)
+            Console.ReadKey()
+        Catch e As Exception
+            MsgBox(e.ToString, MsgBoxStyle.Critical, "ERROR " & Err.Number & " - " & Err.Description)
+        End Try
+    End Sub
+End Module
