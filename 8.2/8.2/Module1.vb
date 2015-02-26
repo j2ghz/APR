be24e2f72cf6e8b5227393945f5a9919401dadaa
Jozef Hollý v17nitro <j2.00ghz@gmail.com>
2015-02-26 09:36:29 +0100

    8.2 hotovo

---
+++ 8.2/8.2/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        vypis(Console.ReadLine)
+    End Sub
+
+    Sub vypis(s As String)
+        If s.Count = 0 Then
+            Console.ReadKey()
+            Exit Sub
+        End If
+        Console.Write(s(s.Length - 1))
+        vypis(s.Substring(0, s.Length - 1))
+    End Sub
+
+End Module
