3eeb3ec34d62ce345a4a1156480a2ea94be99680
Jozef Hollý v17nitro <j2.00ghz@gmail.com>
2015-02-26 08:40:15 +0100

    8.4 start

--- 8.4 Hanojská věž/8.4 Hanojská věž/Module1.vb
+++ 8.4 Hanojská věž/8.4 Hanojská věž/Module1.vb
@@ -1,7 +1,14 @@
 ﻿Module Module1
-
+    Dim A, B, C As New List(Of Integer)
     Sub Main()
+        Console.Write("Zadejte počet disků: ")
+        Dim disky As Integer = Console.ReadLine()
 
+    End Sub
+    Sub presun(ByRef start As List(Of Integer), ByRef cil As List(Of Integer), ByRef pomocny As List(Of Integer), ByVal pocet As Integer)
+        presun(start, pomocny, cil, pocet - 1)
+        cil.Add(start(0)) : start.Remove()
+        presun(pomocny, cil, start, pocet - 1)
     End Sub
 
 End Module
