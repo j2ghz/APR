0aeee1585e934a202e86e2345ed842faf84403c0
Jozef Hollý v17nitro <j2.00ghz@gmail.com>
2015-02-26 09:24:01 +0100

    8.1 hotovo

---
+++ 8.1/8.1/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+    Dim i As Integer = 0
+    Sub Main()
+        Console.Write("Zadejte počet: ")
+        i = Console.ReadLine
+        hvezdicka()
+        Console.ReadKey()
+    End Sub
+
+    Sub hvezdicka()
+        If i > 0 Then
+            Console.WriteLine("*")
+            i -= 1
+            hvezdicka()
+        End If
+    End Sub
+
+End Module
