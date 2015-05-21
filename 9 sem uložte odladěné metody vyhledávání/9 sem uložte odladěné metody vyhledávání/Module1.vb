d19420d9a93c9091ae73c0104d9aa2cef34b0544
Jozef Hollý <j2.00ghz@gmail.com>
2015-05-21 09:28:47 +0200

    9 hotovo

---
+++ 9 sem uložte odladěné metody vyhledávání/9 sem uložte odladěné metody vyhledávání/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+    Dim a As New List(Of String)
+    Sub Main()
+        Console.Write("zadejte prvky oddělené čárkami: ")
+        Dim s As String = Console.ReadLine()
+        Dim b() As String = s.Split(",")
+        a.AddRange(b)
+        Console.WriteLine("Zadejte prvek k vyhledání: ")
+        Console.WriteLine("Index je: " & a.BinarySearch(Console.ReadLine()))
+        Console.ReadKey()
+    End Sub
+
+End Module
