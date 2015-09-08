ec3ea19456a23bc7f968d3b91a4d3a40df4d8db0
Jozef Hollý <j2.00ghz@gmail.com>
2015-09-08 09:39:33 +0200

    pridat scitani

---
+++ scitani/Module1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Text
+Imports System.Text.RegularExpressions
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Zadávejte pouze čísla")
+        Dim cislo1 As New Stack(Of String)(Regex.Split(Console.ReadLine, "").Where(Function(s)
+                                                                                       Return Not String.IsNullOrWhiteSpace(s)
+                                                                                   End Function))
+        Dim cislo2 As New Stack(Of String)(Regex.Split(Console.ReadLine, "").Where(Function(s)
+                                                                                       Return Not String.IsNullOrWhiteSpace(s)
+                                                                                   End Function))
+        Dim vysledek As New StringBuilder
+        Dim prebytek As Byte = 0
+        While (Not cislo1.Count = 0 Or Not cislo2.Count = 0) Or Not prebytek = 0
+            If Not cislo1.Count = 0 Then prebytek += cislo1.Pop
+            If Not cislo2.Count = 0 Then prebytek += cislo2.Pop
+            Dim x = prebytek
+            prebytek = 0
+            While x > 9
+                x -= 10
+                prebytek += 1
+            End While
+            vysledek.Append(x)
+        End While
+        Console.WriteLine(New String(vysledek.ToString.ToCharArray.Reverse.ToArray))
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ scitani/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("scitani")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("scitani")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9a62ce7d-c50f-4ae3-bb73-0802a360d4a4")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
