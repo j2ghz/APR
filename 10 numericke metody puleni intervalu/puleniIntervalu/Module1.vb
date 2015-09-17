11c1a626cd6452c19b03a065fbbc7d0ccf190efd
Jozef Hollý <j2.00ghz@gmail.com>
2015-09-17 13:58:12 +0200

    10

---
+++ 10 numericke metody puleni intervalu/puleniIntervalu/Module1.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim st As New Stopwatch
+        st.Start()
+        Dim a As Decimal = 0
+        Dim b As Decimal = 1
+        Dim last As Decimal
+        Dim fx As Decimal
+        Dim konec As Boolean = False
+        Do
+            Dim fa As Decimal = f(a)
+            Dim fb As Decimal = f(b)
+            Dim x = (a + b) / 2
+            fx = f(x)
+            Select Case Math.Sign(fx)
+                Case Math.Sign(fa)
+                    a = x
+                Case Math.Sign(fb)
+                    b = x
+            End Select
+            konec = last = fx
+            last = fx
+            'Console.WriteLine(String.Join(Environment.NewLine, fx, a, b, x))
+            Console.WriteLine(x)
+        Loop Until konec
+        st.Stop()
+        Console.WriteLine(st.Elapsed)
+        Console.ReadKey()
+    End Sub
+
+    Function f(x As Decimal) As Decimal
+        Return x - Math.Cos(x)
+    End Function
+
+End Module
