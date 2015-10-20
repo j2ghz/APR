b1c01dd99fd88c291ad68ca79ff605cc3d8ac624
Jozef Hollý <j2.00ghz@gmail.com>
2015-10-20 09:41:11 +0200

    ja neviem uš

--- 10 Matice/Matice/Module1.vb
+++ 10 Matice/Matice/Module1.vb
@@ -1,7 +1,14 @@
-﻿Module Module1
+﻿Imports MathNet.Numerics.LinearAlgebra
+
+Module Module1
 
     Sub Main()
-
+        Dim A = Matrix(Of Integer).Build
+        A.Random(3, 4)
+        Dim B = Matrix(Of Integer).Build.Random(4, 3, 2)
+        Dim C = A * B
+        Console.WriteLine(C.ToString)
+        Console.ReadKey()
     End Sub
 
 End Module
---
+++ Malovani/Malování/MainWindow.xaml.vb
@@ -0,0 +1,27 @@
+﻿Imports System.IO
+
+Class MainWindow
+    Dim OFD As New Microsoft.Win32.OpenFileDialog
+    Dim SFD As New Microsoft.Win32.SaveFileDialog
+    Private Sub exit_Click(sender As Object, e As RoutedEventArgs) Handles [exit].Click
+        Me.Close()
+    End Sub
+
+    Private Sub new_Click(sender As Object, e As RoutedEventArgs) Handles [new].Click
+        canvas.Strokes.Clear()
+    End Sub
+
+    Private Sub open_Click(sender As Object, e As RoutedEventArgs) Handles open.Click
+        If OFD.ShowDialog() Then
+            MsgBox("open")
+        End If
+    End Sub
+
+    Private Sub save_Click(sender As Object, e As RoutedEventArgs) Handles save.Click
+        If SFD.ShowDialog Then
+            Dim fs = New FileStream(SFD.FileName, FileMode.Create)
+            canvas.Strokes.Save(fs)
+            fs.Close()
+        End If
+    End Sub
+End Class
