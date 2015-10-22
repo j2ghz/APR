7b1e669173e720d1ea26cdafb626228809e60d05
Jozef Hollý <j2.00ghz@gmail.com>
2015-10-22 13:17:44 +0200

    add paint file save load

--- Malovani/Malování/MainWindow.xaml.vb
+++ Malovani/Malování/MainWindow.xaml.vb
@@ -13,14 +13,26 @@ Class MainWindow
 
     Private Sub open_Click(sender As Object, e As RoutedEventArgs) Handles open.Click
         If OFD.ShowDialog() Then
-            MsgBox("open")
+            Dim bmp As New BitmapImage(New Uri(OFD.FileName))
+            Dim br As New ImageBrush(bmp)
+            br.Stretch = Stretch.None
+            br.AlignmentX = AlignmentX.Left
+            br.AlignmentY = AlignmentY.Top
+            canvas.Height = bmp.Height
+            canvas.Width = bmp.Width
+            canvas.ResizeEnabled = False
+            canvas.Background = br
         End If
     End Sub
 
     Private Sub save_Click(sender As Object, e As RoutedEventArgs) Handles save.Click
         If SFD.ShowDialog Then
             Dim fs = New FileStream(SFD.FileName, FileMode.Create)
-            canvas.Strokes.Save(fs)
+            Dim bitmap As New RenderTargetBitmap(canvas.ActualWidth, canvas.ActualHeight, 96, 96, PixelFormats.Default)
+            bitmap.Render(canvas)
+            Dim encoder As New BmpBitmapEncoder()
+            encoder.Frames.Add(BitmapFrame.Create(bitmap))
+            encoder.Save(fs)
             fs.Close()
         End If
     End Sub
