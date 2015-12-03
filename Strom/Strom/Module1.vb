9456ac85d51517924a7213d731a9fd5847e7b539
Jozef Hollý <j2.00ghz@gmail.com>
2015-12-03 14:12:04 +0100

    Strom done

--- Strom/Strom/Module1.vb
+++ Strom/Strom/Module1.vb
@@ -1,19 +1,20 @@
-﻿Imports System.Security.Policy
-Imports Strom
-
-Module Module1
+﻿Module Module1
 
     Sub Main()
         Console.WriteLine("Zadavejte cisla do stromu, 0 pro konec")
         Dim strom As New Strom
-        Dim a = console.ReadLine()
+        Dim a = Console.ReadLine()
         While a <> 0
             strom.Pridat(a)
-            a = console.ReadLine()
+            a = Integer.Parse(Console.ReadLine())
         End While
-       Console.Write("Vyhledat: ")
+        Console.WriteLine("===================================")
+        strom.Prochazej()
+        Console.WriteLine("===================================")
+        Console.Write("Vyhledat: ")
         Dim x As Integer = Console.ReadLine()
-
+        strom.Najdi(x)
+        Console.ReadLine()
     End Sub
 
 End Module
@@ -27,21 +28,53 @@ Class Strom
         End If
     End Sub
 
-     Sub Pridat(hodnota As Integer, uzel As Uzel)
-        select Case uzel.Hodnota
-            Case <hodnota
-                If uzel.Levy Is Nothing then
-                    uzel.Levy = new Uzel(hodnota)
-                    Else
-                    Pridat(hodnota,uzel.Levy)
+    Sub Pridat(hodnota As Integer, uzel As Uzel)
+        Select Case uzel.Hodnota
+            Case < hodnota
+                If uzel.Levy Is Nothing Then
+                    uzel.Levy = New Uzel(hodnota)
+                Else
+                    Pridat(hodnota, uzel.Levy)
                 End If
-                Case > hodnota
-                If uzel.pravy Is Nothing then
-                    uzel.pravy = new Uzel(hodnota)
-                    Else
-                    Pridat(hodnota,uzel.pravy)
+            Case > hodnota
+                If uzel.Pravy Is Nothing Then
+                    uzel.Pravy = New Uzel(hodnota)
+                Else
+                    Pridat(hodnota, uzel.Pravy)
                 End If
         End Select
+    End Sub
+
+    Public Sub Najdi(x As Integer)
+        Najdi(x, Koren)
+    End Sub
+
+    Private Sub Najdi(x As Integer, uzel As Uzel)
+        If uzel Is Nothing Then
+            Console.WriteLine("Nenalezeno")
+        Else
+            Console.WriteLine(uzel.Hodnota)
+            Select Case uzel.Hodnota
+                Case x
+                    Console.WriteLine("Nalezeno")
+                Case < x
+                    Najdi(x, uzel.Levy)
+                Case > x
+                    Najdi(x, uzel.Pravy)
+            End Select
+        End If
+    End Sub
+
+    Public Sub Prochazej()
+        Prochazej(Koren)
+    End Sub
+
+    Sub Prochazej(uzel As Uzel)
+        If uzel IsNot Nothing Then
+            Prochazej(uzel.Levy)
+            Console.WriteLine(uzel.Hodnota)
+            Prochazej(uzel.Pravy)
+        End If
     End Sub
 End Class
 Class Uzel
