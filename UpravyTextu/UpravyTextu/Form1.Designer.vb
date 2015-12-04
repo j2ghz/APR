1e56a8b58319680412f3d2c99aead5af092c8ab3
Jozef Hollý <j2.00ghz@gmail.com>
2015-12-04 12:25:01 +0100

    vytvorit upravy textu

---
+++ UpravyTextu/UpravyTextu/Form1.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SouborToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NovyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OtevritToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UlozitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ZavritToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UpravyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OtocitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NaVelkaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NaVelkaToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NaMalaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SifraToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabelCarky = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabelSlova = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SouborToolStripMenuItem, Me.UpravyToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(645, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'SouborToolStripMenuItem
+        '
+        Me.SouborToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NovyToolStripMenuItem, Me.OtevritToolStripMenuItem, Me.UlozitToolStripMenuItem, Me.ZavritToolStripMenuItem})
+        Me.SouborToolStripMenuItem.Name = "SouborToolStripMenuItem"
+        Me.SouborToolStripMenuItem.Size = New System.Drawing.Size(57, 20)
+        Me.SouborToolStripMenuItem.Text = "Soubor"
+        '
+        'NovyToolStripMenuItem
+        '
+        Me.NovyToolStripMenuItem.Name = "NovyToolStripMenuItem"
+        Me.NovyToolStripMenuItem.Size = New System.Drawing.Size(110, 22)
+        Me.NovyToolStripMenuItem.Text = "Novy"
+        '
+        'OtevritToolStripMenuItem
+        '
+        Me.OtevritToolStripMenuItem.Name = "OtevritToolStripMenuItem"
+        Me.OtevritToolStripMenuItem.Size = New System.Drawing.Size(110, 22)
+        Me.OtevritToolStripMenuItem.Text = "Otevrit"
+        '
+        'UlozitToolStripMenuItem
+        '
+        Me.UlozitToolStripMenuItem.Name = "UlozitToolStripMenuItem"
+        Me.UlozitToolStripMenuItem.Size = New System.Drawing.Size(110, 22)
+        Me.UlozitToolStripMenuItem.Text = "Ulozit"
+        '
+        'ZavritToolStripMenuItem
+        '
+        Me.ZavritToolStripMenuItem.Name = "ZavritToolStripMenuItem"
+        Me.ZavritToolStripMenuItem.Size = New System.Drawing.Size(110, 22)
+        Me.ZavritToolStripMenuItem.Text = "Zavrit"
+        '
+        'UpravyToolStripMenuItem
+        '
+        Me.UpravyToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OtocitToolStripMenuItem, Me.NaVelkaToolStripMenuItem, Me.SifraToolStripMenuItem})
+        Me.UpravyToolStripMenuItem.Name = "UpravyToolStripMenuItem"
+        Me.UpravyToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.UpravyToolStripMenuItem.Text = "Upravy"
+        '
+        'OtocitToolStripMenuItem
+        '
+        Me.OtocitToolStripMenuItem.Name = "OtocitToolStripMenuItem"
+        Me.OtocitToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.OtocitToolStripMenuItem.Text = "Otocit"
+        '
+        'NaVelkaToolStripMenuItem
+        '
+        Me.NaVelkaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NaVelkaToolStripMenuItem1, Me.NaMalaToolStripMenuItem})
+        Me.NaVelkaToolStripMenuItem.Name = "NaVelkaToolStripMenuItem"
+        Me.NaVelkaToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.NaVelkaToolStripMenuItem.Text = "Prevest"
+        '
+        'NaVelkaToolStripMenuItem1
+        '
+        Me.NaVelkaToolStripMenuItem1.Name = "NaVelkaToolStripMenuItem1"
+        Me.NaVelkaToolStripMenuItem1.Size = New System.Drawing.Size(119, 22)
+        Me.NaVelkaToolStripMenuItem1.Text = "Na velka"
+        '
+        'NaMalaToolStripMenuItem
+        '
+        Me.NaMalaToolStripMenuItem.Name = "NaMalaToolStripMenuItem"
+        Me.NaMalaToolStripMenuItem.Size = New System.Drawing.Size(119, 22)
+        Me.NaMalaToolStripMenuItem.Text = "Na mala"
+        '
+        'SifraToolStripMenuItem
+        '
+        Me.SifraToolStripMenuItem.Name = "SifraToolStripMenuItem"
+        Me.SifraToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.SifraToolStripMenuItem.Text = "Sifra"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabelCarky, Me.ToolStripStatusLabelSlova})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 487)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(645, 22)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabelCarky
+        '
+        Me.ToolStripStatusLabelCarky.Name = "ToolStripStatusLabelCarky"
+        Me.ToolStripStatusLabelCarky.Size = New System.Drawing.Size(80, 17)
+        Me.ToolStripStatusLabelCarky.Text = "Pocet carek: 0"
+        '
+        'ToolStripStatusLabelSlova
+        '
+        Me.ToolStripStatusLabelSlova.Name = "ToolStripStatusLabelSlova"
+        Me.ToolStripStatusLabelSlova.Size = New System.Drawing.Size(92, 17)
+        Me.ToolStripStatusLabelSlova.Text = "Nejdelsi slovo: 0"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(0, 24)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(645, 463)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(645, 509)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabelCarky As ToolStripStatusLabel
+    Friend WithEvents SouborToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UpravyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OtocitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NaVelkaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NaVelkaToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents NaMalaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripStatusLabelSlova As ToolStripStatusLabel
+    Friend WithEvents NovyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OtevritToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UlozitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ZavritToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SifraToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ UpravyTextu/UpravyTextu/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Update() Handles RichTextBox1.TextChanged
+        ToolStripStatusLabelCarky.Text = "Pocet carek: " & RichTextBox1.
+    End Sub
+End Class
